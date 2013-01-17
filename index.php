44666a2ba7e2a8dc211662901992b76c8f244a1b
Jj <hansoncao@live.com>
2013-01-18 00:55:33 +0800

    url

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>About</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/layout.css" type="text/css" media="screen"> 
+    <script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+    <script src="js/cufon-yui.js" type="text/javascript"></script>
+    <script src="js/cufon-replace.js" type="text/javascript"></script> 
+    <script src="js/Dynalight_400.font.js" type="text/javascript"></script>
+    <script src="js/FF-cash.js" type="text/javascript"></script>
+    <script src="js/tms-0.3.js" type="text/javascript"></script>
+    <script src="js/tms_presets.js" type="text/javascript"></script>
+    <script src="js/jquery.easing.1.3.js" type="text/javascript"></script>
+    <script src="js/jquery.equalheights.js" type="text/javascript"></script>    
+	<!--[if lt IE 8]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+        	<img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+        </a>
+    </div>
+	<![endif]-->
+    <!--[if lt IE 9]>
+   		<script type="text/javascript" src="js/html5.js"></script>
+	<![endif]-->
+</head>
+<body id="page1">
+	<!--==============================header=================================-->
+    <header>
+    	<div class="row-top">
+        	<div class="main">
+            	<div class="wrapper">
+                	<h1><a href="index.html">Catering<span>.com</span></a></h1>
+                    <nav>
+                       <ul class="menu">
+                            <li><a class="active" href="index.html">Home</a></li>
+                            <li><a href="menu.html">Menu</a></li>
+                            <li><a href="catalogue.html">Gallery </a></li>
+                            <li><a href="shipping.html">Contact</a></li>
+                            
+                            
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="row-bot">
+        	<div class="row-bot-bg">
+            	<div class="main">
+                	<h2>Impressive Selection <span>for any Occasion</span></h2>
+                    <div class="slider-wrapper">
+                        <div class="slider">
+                            <ul class="items">
+                                <li>
+                                    <img src="images/slider-img1.jpg" alt="" />
+                                </li>
+                                <li>
+                                    <img src="images/slider-img2.jpg" alt="" />
+                                </li>
+                                <li>
+                                    <img src="images/slider-img3.jpg" alt="" />
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+	<!--==============================content================================-->
+    <section id="content"><div class="ic">More Website Templates @ TemplateMonster.com - January 23, 2012!</div>
+        <div class="main">
+            <div class="wrapper img-indent-bot">
+                <article class="col-1">
+                	<a href="#"><img class="img-border" src="images/banner-1.jpg" alt=""></a>
+                </article>
+                <article class="col-1">
+                	<a href="#"><img class="img-border" src="images/banner-2.jpg" alt=""></a>
+                </article>
+                <article class="col-2">
+                	<a href="#"><img class="img-border" src="images/banner-3.jpg" alt=""></a>
+                </article>
+            </div>
+            <div class="wrapper">
+            	<article class="column-1">
+                	<div class="indent-left">
+                        <div class="maxheight indent-bot">
+                            <h3>Openning Hours</h3>
+                            <ul class="list-1">
+                                <li>Monday: 10AM -9 PM</li>
+                                <li>Tuesday: 10AM -9 PM</li>
+                                <li>Wednesday: 10AM -9 PM</li>
+                                <li>Thursday: 10AM -9 PM</li>
+                                <li>Friday: 10AM -9 PM</li>
+                                <li>Saturday: 10AM -9 PM</li>
+                                <li>Sunday: 10AM -9 PM </li>
+                            </ul>
+                        </div>
+                       
+                    </div>
+                </article>
+                <article class="column-2">
+                	<div class="maxheight indent-bot">
+                        <h3>Our Services</h3>
+                            <ul class="list-1">
+                                <li>Dinne in</li>
+                                <li>Hendrerit in vulputate velit esse molestie </li>
+                                <li>Take Away</li>
+                                <li>Delivery</li>
+                                
+                            </ul>
+                       </div>
+                   
+                </article>
+                <article class="column-2">
+                	<div class="maxheight indent-bot">
+                        <h3>Reservations</h3>
+                            <ul class="list-1">
+                                <li>Lunch:11:00am -  4:30pm </li>
+                                <li>Dinner:4:30pm - 10:30pm </li>
+                                <li>Promotion:  Wednesday </li> 
+                            </ul>
+                       </div>
+                   
+                </article>
+            </div>
+        </div>
+    </section>
+    
+	<!--==============================footer=================================-->
+    <footer>
+        <div class="main">
+        	<div class="aligncenter">
+            	<span>Catering &copy; 2013 <a rel="nofollow" class="link" target="_blank" href="http://www.templatemonster.com/">by ahaic </a></span>
+                  
+            </div>
+        </div>
+    </footer>
+    
+    
+    <script type="text/javascript"> Cufon.now(); </script>
+    <script type="text/javascript">
+		$(window).load(function() {
+			$('.slider')._TMS({
+				duration:1000,
+				easing:'easeOutQuint',
+				preset:'slideDown',
+				slideshow:7000,
+				banners:false,
+				pauseOnHover:true,
+				pagination:true,
+				pagNums:false
+			});
+		});
+    </script>
+</body>
+</html>
